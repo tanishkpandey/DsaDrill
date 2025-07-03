28469b4a6fa22c7d1fecc676d7724748c287f493
tanishkpandey <pandeytanishk@gmail.com>
2025-07-03 09:13:45 +0530

    sum of numbers till single digit

---
+++ 3. Basic Math Problems/1.SumOfNumberTillSingleDigit.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int SumOfNumber(int n){
+    int sum=0;
+    while(n!=0){
+        int digit = n%10;
+        sum = sum + digit;
+        n=n/10;
+    }
+    return sum;
+}
+
+int main() {
+int sum = SumOfNumber(118);
+if(sum>9){
+    SumOfNumber(sum);
+    cout<<"The sum to single Digit is: "<<SumOfNumber(sum);
+} else{
+   cout<<"The sum to single Digit is: "<<sum;
+}
+    return 0;
+}
