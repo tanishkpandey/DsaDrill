77a975c128d5bf56049d01a888d4e4ae3b83348b
tanishkpandey <pandeytanishk@gmail.com>
2025-07-09 09:55:19 +0530

    search an element in array

--- 4. Array/3.SearchforanElementinanArray.cpp
+++ 4. Array/3.SearchforanElementinanArray.cpp
@@ -1,1 +1,10 @@
-Search for an Element in an Array
+int searchElementInArray(vector<int>& nums, int target) {
+    int size = nums.size();
+    for(int i=0; i<size; i++){
+        if(nums[i]== target){
+            return i;
+        }
+    }
+
+    return -1; // Placeholder return
+}
